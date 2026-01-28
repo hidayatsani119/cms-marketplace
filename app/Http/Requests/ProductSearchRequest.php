e526f46b700e93489ad70b1d43b4941d36120488
L .Nasrullah Hidayat Sani <hidayatsani119@gmail.com>
2026-01-28 14:32:34 +0700

    fix : ProductSearchRequest validation errors status code to 400

--- app/Http/Requests/ProductSearchRequest.php
+++ app/Http/Requests/ProductSearchRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ProductSearchRequest extends FormRequest
 {
@@ -25,5 +27,12 @@ class ProductSearchRequest extends FormRequest
             'name' => ['nullable', 'string', 'max:100'],
             'order' => ['nullable', 'in:asc,desc'],
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            "errors" => $validator->getMessageBag()
+        ],400));
     }
 }
